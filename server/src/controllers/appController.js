89241f1b06277a6469d28e08a78f20b35e242215
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-30 15:08:14 +0300

    get my furnitures

--- server/src/controllers/appController.js
+++ server/src/controllers/appController.js
@@ -2,8 +2,15 @@ const router = require('express').Router();
 const appService = require('../services/appService');
 
 router.get('/catalog', async (req, res) => {
-    const allFurnitures = await appService.getAll();
-    res.json(allFurnitures);
+    if (req.query.where) {
+        let userId = req.query.where.split('=')[1];
+        userId = userId.substring(1, userId.length - 1);
+        const allMy = await appService.myFurnitures(userId);
+        res.json(allMy);
+    } else {
+        const allFurnitures = await appService.getAll();
+        res.json(allFurnitures);
+    }
 });
 
 router.post('/catalog', async (req, res) => {
@@ -11,7 +18,14 @@ router.post('/catalog', async (req, res) => {
     const token = req.headers['x-authorization'];
     const decodedToken = await appService.decodeTokenService(token);
     const createdFurniture = await appService.createFurniture({ ...data, _ownerId: decodedToken._id });
-    res.json(createdFurniture); 
+    res.json(createdFurniture);
 });
+
+router.get('/catalog/:id', async (req, res) => {
+    const furniture = await appService.getOne(req.params.id);
+    res.json(furniture);
+});
+
+
 
 module.exports = router;
--- server/src/services/appService.js
+++ server/src/services/appService.js
@@ -5,5 +5,9 @@ const { SECRET } = require('../../config/constants');
 const jwtVerify = promisify(jwt.verify);
 
 exports.getAll = async () => await Furniture.find();
+exports.getOne = async (id) => await Furniture.findById({ _id: id });
 exports.createFurniture = async (adData) => await Furniture.create(adData);
 exports.decodeTokenService = async (token) => await jwtVerify(token, SECRET);
+exports.editFurniture = async (id, data) => await Furniture.findByIdAndUpdate(id, data);
+exports.deleteFurniture = async (id) => await Furniture.findByIdAndDelete(id);
+exports.myFurnitures = async (id) => await Furniture.find({ _ownerId: id });
